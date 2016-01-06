5d55bf0a0dff791cc2f18b3c47e47435d7190af1
Geoff Jacobsen <geoffjacobsen@gmail.com>
2016-01-06 19:12:00 +1300

    Fix compiler
    Fix loading plugin resource more than once
    
    Fix not detecting require dependency in the form:
      require('foo').plus('bar');

--- test/compiler-tests.js
+++ test/compiler-tests.js
@@ -5,11 +5,11 @@ var compiler = require('../lib/compiler');
 
 var config = {
   baseUrl: path.resolve(__dirname),
-  name: "data/compile-top"
 };
 
 describe("Compiling modules", function () {
   it("can combine modules", function (done) {
+    config.name =  "data/compile-top";
     compiler.compile(config, function (codeTree) {
       expect(codeTree.code).to.match(/define\("data\/dep2",\["require","exports","module"\], function/); codeTree = codeTree.next;
       expect(codeTree.code).to.match(/define\("data\/subdir\/dep1",\["require","exports","module","data\/dep2"\], function/); codeTree = codeTree.next;
@@ -19,6 +19,18 @@ describe("Compiling modules", function () {
       expect(codeTree.code).to.match(/define\("data\/complex-plugin",{/); codeTree = codeTree.next;
       expect(codeTree.code).to.match(/define\("data\/simple-plugin!data\/fuzz","simple data\/fuzz"\);/); codeTree = codeTree.next;
       expect(codeTree.code).to.match(/define\("data\/simple-plugin",\["require","exports","module"\], function/); codeTree = codeTree.next;
+      done();
+    });
+  });
+
+  it("can detech require dependencies", function (done) {
+    config.name = "data/compile-deps";
+    compiler.compile(config, function (codeTree) {
+      expect(codeTree.code).to.match(/define\("data\/compile-deps-1",\["require","exports","module"\], function/);
+      expect((codeTree = codeTree.next).code).to.match(/define\("data\/compile-deps",\["require","exports","module","data\/norm-plugin!norm\/one","data\/compile-deps-1"\], function/);
+      expect((codeTree = codeTree.next).code).to.match(/define\("data\/norm-plugin!norm\/one"/); // should only be one of these
+      expect((codeTree = codeTree.next).code).to.match(/define\("data\/norm-plugin"/);
+      expect((codeTree = codeTree.next).code).to.be(undefined);
       done();
     });
   });
