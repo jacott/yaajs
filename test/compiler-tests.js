4d29fec8fab91f0b4ac7fcb1ba7b6d3c5a3a38c2
Geoff Jacobsen <geoffjacobsen@gmail.com>
2019-09-11 14:33:08 +1200

    update terser to 4.3.1

--- test/compiler-tests.js
+++ test/compiler-tests.js
@@ -17,17 +17,17 @@ describe("Compiling modules", ()=>{
     compiler.compile({name: "data/compile-top", contextConfig, callback(r) {ast = r.ast}});
     assert.equal(ast.TYPE, "Toplevel");
     assertAst(ast, [
-      'define("data/simple-plugin",["require","exports","module"],function(require,exports,module){return{load:function(name,req,onLoad){onLoad(req.module.state===module.constructor.PREPARING?"simple "+name:"not preparing")},write:function(pluginName,name,write){write("define("+JSON.stringify(pluginName+"!"+name)+","+JSON.stringify("simple "+name)+");\\n")}}});',
+      'define("data/simple-plugin",["require","exports","module"],(function(require,exports,module){return{load:function(name,req,onLoad){onLoad(req.module.state===module.constructor.PREPARING?"simple "+name:"not preparing")},write:function(pluginName,name,write){write("define("+JSON.stringify(pluginName+"!"+name)+","+JSON.stringify("simple "+name)+");\\n")}}}));',
 
       'define("data/simple-plugin!data/fuzz","simple data/fuzz");',
 
-      'define("data/complex-plugin",{load:function(name,req,onload,config){req(name,function(value,pMod){pMod.addDependancy(req.module);onload(value)},onload.error)},pluginBuilder:"./complex-plugin-builder"});',
-      'define("data/simple",["require","exports","module"],function(require,exports,module){return"simple"});',
-      'define("data/complex-plugin!data/simple",["data/simple"],function(client){return client});',
+      'define("data/complex-plugin",{load:function(name,req,onload,config){req(name,(function(value,pMod){pMod.addDependancy(req.module);onload(value)}),onload.error)},pluginBuilder:"./complex-plugin-builder"});',
+      'define("data/simple",["require","exports","module"],(function(require,exports,module){return"simple"}));',
+      'define("data/complex-plugin!data/simple",["data/simple"],(function(client){return client}));',
 
       'define("data/dep2",["require","exports","module"],(require,exports,module)=>true);',
-      'define("data/subdir/dep1",["require","exports","module","data/dep2"],function(require,exports,module){var dep2=require("../dep2");module.onUnload(function(){dep1.testUnload=true});var count=0;return dep1;function dep1(){var name="data/dep2";return dep2===require(name)&&++count===1}});',
-      '(function(){define("data/compile-top",["require","exports","data/subdir/dep1","data/simple-plugin!data/fuzz","data/complex-plugin!data/simple"],function(require,exports,dep1,fuzz){var util=require("data/dep2")})})();',
+      'define("data/subdir/dep1",["require","exports","module","data/dep2"],(function(require,exports,module){var dep2=require("../dep2");module.onUnload((function(){dep1.testUnload=true}));var count=0;return dep1;function dep1(){var name="data/dep2";return dep2===require(name)&&++count===1}}));',
+      '(function(){define("data/compile-top",["require","exports","data/subdir/dep1","data/simple-plugin!data/fuzz","data/complex-plugin!data/simple"],(function(require,exports,dep1,fuzz){var util=require("data/dep2")}))})();',
     ]);
 
     assert.equal(ast.body.length, 8);
@@ -43,9 +43,9 @@ describe("Compiling modules", ()=>{
       `?factory(exports):typeof define==="function"&&define.amd?`,
       `define("data/funny-define",["exports"],factory):`,
       `factory(global.d3=global.d3||{})})`,
-      `(this,function(exports){exports.foo=123;`,
+      `(this,(function(exports){exports.foo=123;`,
       `function define(){}const define=()=>{};`,
-      `let a,b,c;define(a,b,c)});`
+      `let a,b,c;define(a,b,c)}));`
     ]);
   });
 
@@ -65,11 +65,11 @@ describe("Compiling modules", ()=>{
     assert.equal(ast.TYPE, "Toplevel");
     assert.equal(ast.body.length, 4);
     assertAst(ast, [
-      'define("data/norm-plugin",["require","exports","module"],function(require,exports,module){return{normalize:function(name,parent){return"norm/"+name.split("/")[0]},load:function(name,req,onLoad){onLoad("hello "+name)},write:function(pluginName,name,write){write("define("+JSON.stringify(pluginName+"!"+name)+","+JSON.stringify("norm "+name)+");\\n")}}});',
+      'define("data/norm-plugin",["require","exports","module"],(function(require,exports,module){return{normalize:function(name,parent){return"norm/"+name.split("/")[0]},load:function(name,req,onLoad){onLoad("hello "+name)},write:function(pluginName,name,write){write("define("+JSON.stringify(pluginName+"!"+name)+","+JSON.stringify("norm "+name)+");\\n")}}}));',
       'define("data/norm-plugin!norm/one","norm norm/one");',
 
-      'define("data/compile-deps-1",["require","exports","module"],function(require,exports,module){return{define:function(fooBar){return"defined: "+fooBar}}});',
-      'define("data/compile-deps",["require","exports","module","data/norm-plugin!norm/one","data/compile-deps-1"],function(require,exports,module){var name="./other";require(name);require(["./fuzz"]);require("./foo",function(){});name.require("./baz");require("./norm-plugin!one/load");require("./norm-plugin!one/more/load");return require("./compile-deps-1").define("x")});',
+      'define("data/compile-deps-1",["require","exports","module"],(function(require,exports,module){return{define:function(fooBar){return"defined: "+fooBar}}}));',
+      'define("data/compile-deps",["require","exports","module","data/norm-plugin!norm/one","data/compile-deps-1"],(function(require,exports,module){var name="./other";require(name);require(["./fuzz"]);require("./foo",(function(){}));name.require("./baz");require("./norm-plugin!one/load");require("./norm-plugin!one/more/load");return require("./compile-deps-1").define("x")}));',
     ]);
   });
 
@@ -102,10 +102,10 @@ describe("Compiling modules", ()=>{
       `define("data/dep2",["require","exports","module"],(require,exports,module)=>true);`
     ]);
     assertAst(groups[1], [
-      `define("data/subdir/dep1",["require","exports","module","data/dep2"],function(require,exports,module){var dep2=require("../dep2");module.onUnload(function(){dep1.testUnload=true});var count=0;return dep1;function dep1(){var name="data/dep2";return dep2===require(name)&&++count===1}});`
+      `define("data/subdir/dep1",["require","exports","module","data/dep2"],(function(require,exports,module){var dep2=require("../dep2");module.onUnload((function(){dep1.testUnload=true}));var count=0;return dep1;function dep1(){var name="data/dep2";return dep2===require(name)&&++count===1}}));`
     ]);
     assertAst(groups[2], [
-      `define("data/dep3",["require","exports","module","data/dep2"],function(require,exports,module){const dep2=require("./dep2");return true});`
+      `define("data/dep3",["require","exports","module","data/dep2"],(function(require,exports,module){const dep2=require("./dep2");return true}));`
     ]);
     assert.equal(groups.length, 3);
   });
